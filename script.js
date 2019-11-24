f686d17dea6c57587c1d348463cfc041d3152d19
Akshay Sharma <aakshayvashishth@gmail.com>
2019-11-24 15:44:24 -0600

    update2

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let login = false;
+let closedp = true;
+
+function closeEverything()
+{
+    if(closedp){
+        document.getElementById("inputDp").style.display="none";
+    }
+    else{
+        closedp = true;
+    }
+}
 
 function openNav() {
 document.getElementById("sideNav").style.width = "24%";
@@ -18,6 +29,39 @@ function setlogout()
     login = false;
     accDisplaySelect();
 }
+
+
+function displayMatches()
+{
+
+    let input, filterUC, list, items, a, txt;
+    input = document.getElementById("input");
+    list = document.getElementById("books");
+    items = list.getElementsByTagName("li");
+    if(input.value!="")
+    {
+        document.getElementById("inputDp").style.display="block";
+        inputUC = input.value.toUpperCase();
+        for (let i = 0; i < items.length; i++) {
+            a = items[i].getElementsByTagName("a")[0];
+            txt = a.textContent || a.innerText;
+            if (txt.toUpperCase().indexOf(inputUC) > -1) {
+                items[i].style.display = "block";
+            } else {
+                items[i].style.display = "none";
+            }
+        }
+    }
+    else{
+        document.getElementById("inputDp").style.display="none";
+        for(let i = 0; i < items.length; i++)
+        {
+            items[i].style.display = "none";
+        }
+    }
+    closedp = false;
+}
+
 
 function accDisplaySelect()
 {
